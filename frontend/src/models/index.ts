4b9a54d3c87925f6d7cf1aa2a77931dc55d5cd41
NeeharGN <92006103+NeeharGN@users.noreply.github.com>
2025-10-13 19:55:14 +0530

    Updating the repository

--- frontend/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './auth';
-export * from './yield';
-export * from './worker';
-export * from './financial';
-export * from './fertiliser';
-export * from './weather';
--- frontend/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export { authService } from './authService';
-export { yieldService } from './yieldService';
-export { workerService } from './workerService';
-export { financialService } from './financialService';
-export { fertiliserService } from './fertiliserService';
-export { weatherService } from './weatherService';
